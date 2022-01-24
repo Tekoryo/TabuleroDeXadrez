4d5ae64b3f626a34c0df760e1ecd4fe5e0255f4e
Thiago C. Santana <thiagocoelho.coelho2@gmail.com>
2022-01-24 15:57:45 -0300

    Criacao do Board e Piece

---
+++ src/board/Piece.java
@@ -0,0 +1,17 @@
+package board;
+
+public class Piece {
+    private Board board;
+    protected Position position;
+
+    public Piece(Board board) {
+        this.board = board;
+        this.position=null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+    
+  
+}
